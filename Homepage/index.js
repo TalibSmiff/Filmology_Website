ef84bb4c71c41eb8fb082718e4c76967cd145943
Talibsmiff <talibsmith77@gmail.com>
2026-02-22 16:49:21 -0500

    Initial commit - Filmology website

---
+++ Homepage/index.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const images = document.querySelectorAll('img');
+    const scrollY = window.scrollY;
+    
+    images.forEach(img => {
+        const blur = scrollY / 20;
+        img.style.filter = `blur(${blur}px)`;
+    });
+});
