9ac7c70b46e846f1e73af6abe6337d8c78f4d449
Talibsmiff <talibsmith77@gmail.com>
2026-02-22 17:49:46 -0500

    uncommw

--- Homepage/index.js
+++ Homepage/index.js
@@ -1,9 +1,9 @@
-window.addEventListener('scroll', function() {
-    const images = document.querySelectorAll('img');
-    const scrollY = window.scrollY;
+// window.addEventListener('scroll', function() {
+//     const images = document.querySelectorAll('img');
+//     const scrollY = window.scrollY;
     
-    images.forEach(img => {
-        const blur = scrollY / 20;
-        img.style.filter = `blur(${blur}px)`;
-    });
-});
+//     images.forEach(img => {
+//         const blur = scrollY / 20;
+//         img.style.filter = `blur(${blur}px)`;
+//     });
+// });
