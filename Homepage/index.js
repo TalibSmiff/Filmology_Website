5059cce5d198590833f8dd21b26c6a5474770ce8
Talibsmiff <talibsmith77@gmail.com>
2026-03-01 01:41:21 -0500

    removed js

--- Homepage/index.js
+++
@@ -1,9 +0,0 @@
-// window.addEventListener('scroll', function() {
-//     const images = document.querySelectorAll('img');
-//     const scrollY = window.scrollY;
-    
-//     images.forEach(img => {
-//         const blur = scrollY / 20;
-//         img.style.filter = `blur(${blur}px)`;
-//     });
-// });
